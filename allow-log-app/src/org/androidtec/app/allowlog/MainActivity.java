7ec8137ee73d6f3c7e818d85d3c18e4ed81456f4
mike_neck <jkrt3333@gmail.com>
2012-01-25 00:57:28 +0900

    DEVSUMI-9 modify main.xml via https://atec.backlog.jp/view/DEVSUMI-9

--- allow-log-app/src/org/androidtec/app/allowlog/MainActivity.java
+++ allow-log-app/src/org/androidtec/app/allowlog/MainActivity.java
@@ -1,15 +1,19 @@
+
 package org.androidtec.app.allowlog;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class MainActivity extends Activity {
+
+	/**
+	 * @param savedInstanceState
+	 *            - the status before this method was called in this
+	 *            application.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
