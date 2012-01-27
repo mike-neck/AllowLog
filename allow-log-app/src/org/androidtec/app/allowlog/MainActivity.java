2a3ac7ea9e0e91eb87ac678629ec411f9184dbe1
mike_neck <jkrt3333@gmail.com>
2012-01-27 22:58:07 +0900

    DEVSUMI-9 implement database

--- allow-log-app/src/org/androidtec/app/allowlog/MainActivity.java
+++ allow-log-app/src/org/androidtec/app/allowlog/MainActivity.java
@@ -1,10 +1,19 @@
 
 package org.androidtec.app.allowlog;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Calendar;
+import java.util.TimeZone;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
 
 	/**
 	 * @param savedInstanceState
@@ -15,5 +24,83 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		View historyButton = findViewById(R.id.to_history);
+		historyButton.setOnClickListener(this);
+
+		View registerButton = findViewById(R.id.allowance_registration);
+		registerButton.setOnClickListener(this);
+
+		SQLiteDatabase sqLiteDatabase = prepareSql();
+		String sql = countSql();
+		long totalValue = getTotalAmount(sqLiteDatabase, sql);
+		TextView total = (TextView) findViewById(R.id.total);
+		total.setText(Long.toString(totalValue));
+	}
+
+	long getTotalAmount(SQLiteDatabase sqLiteDatabase, String sql) {
+		Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
+		cursor.moveToFirst();
+		int count = cursor.getCount();
+		if ( count > 0 ) {
+			long totalValue = cursor.getLong(0);
+			return totalValue;
+		} else {
+			return 0L;
+		}
+	}
+
+	private SQLiteDatabase prepareSql() {
+		SQLiteOpenHelper database = new AllowanceDatabase(this);
+		return database.getReadableDatabase();
+	}
+
+	private String countSql() {
+		StringBuilder builder = new StringBuilder();
+		long from = getFromDate();
+		long to = getToDate();
+		return builder.append("SELECT ").append("SUM(AMOUNT) ").append("FROM ").append("ALLOWANCE_LOG ")
+		        .append("WHERE LOG_DATE BETWEEN ").append(from).append(" AND ").append(to).append(";").toString();
+	}
+
+	private long getToDate() {
+		Calendar toCalendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+		toCalendar.add(Calendar.MONTH, 1);
+		toCalendar.set(Calendar.DATE, 1);
+		toCalendar.set(Calendar.HOUR, 0);
+		toCalendar.set(Calendar.MINUTE, 0);
+		toCalendar.set(Calendar.SECOND, 0);
+		toCalendar.set(Calendar.MILLISECOND, 0);
+		toCalendar.add(Calendar.MILLISECOND, -1);
+		return toCalendar.getTime().getTime();
+	}
+
+	private long getFromDate() {
+		Calendar fromCalendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+		fromCalendar.set(Calendar.DATE, 1);
+		fromCalendar.set(Calendar.HOUR, 0);
+		fromCalendar.set(Calendar.MINUTE, 0);
+		fromCalendar.set(Calendar.SECOND, 0);
+		fromCalendar.set(Calendar.MILLISECOND, 0);
+		return fromCalendar.getTime().getTime();
+	}
+
+	/**
+	 * @param view
+	 *            - the View user clicked.
+	 */
+	@Override
+	public void onClick(View view) {
+		int id = view.getId();
+		switch ( id ) {
+		case R.id.to_history:
+
+			break;
+		case R.id.allowance_registration:
+
+			break;
+		default:
+			break;
+		}
 	}
 }
