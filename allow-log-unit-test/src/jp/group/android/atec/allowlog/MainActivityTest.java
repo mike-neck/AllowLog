f2dcf125781ff2bbff21c184f7e70476641fe5a9
mike_neck <jkrt3333@gmail.com>
2012-02-05 06:33:08 +0900

    DEVSUMI-20 destructive commit --- tests will get error after this commit

--- allow-log-unit-test/src/jp/group/android/atec/allowlog/MainActivityTest.java
+++ allow-log-unit-test/src/jp/group/android/atec/allowlog/MainActivityTest.java
@@ -1,21 +1,69 @@
 
 package jp.group.android.atec.allowlog;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
- * This is a simple framework for a test of an Application. See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more
- * information on how to write and extend Application tests.
- * <p/>
- * To run this test, you can type: adb shell am instrument -w \ -e class
- * jp.group.android.atec.allowlog.MainActivityTest \
- * jp.group.android.atec.allowlog.tests/android.test.InstrumentationTestRunner
+ * 初回起動時のViewに関するテスト<br/>
+ *
+ * <ul>
+ *     <li>使用金額が０円</li>
+ *     <li>今月使用された金額が０円</li>
+ * </ul>
+ * @author mike_neck
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+    private static final Class<MainActivity> CLASS = MainActivity.class;
+
+    private Activity activity;
+
+    private Instrumentation instrumentation;
+
+    private static final String TABLE = "ALLOWANCE_LOG";
+
     public MainActivityTest() {
-        super("jp.group.android.atec.allowlog", MainActivity.class);
+        super(CLASS);
+    }
+
+    @Override
+    public void setUp () {
+        activity = getActivity();
+        instrumentation = getInstrumentation();
+        setActivityInitialTouchMode(false);
+
+        SQLiteOpenHelper sqLiteOpenHelper = getAllowanceDatabase();
+        SQLiteDatabase database = sqLiteOpenHelper.getWritableDatabase();
+
+        database.beginTransaction();
+        database.delete(TABLE, null, null);
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        database.close();
+    }
+
+    public void test最初に表示される金額は０円 () {
+        instrumentation.waitForIdleSync();
+        EditText editText = (EditText) activity.findViewById(R.id.payment);
+        String actual = editText.getText().toString();
+        assertEquals("", actual);
+    }
+
+    public void test今月はまだ一度も使っていないので０円 () {
+        instrumentation.waitForIdleSync();
+        TextView textView = (TextView) activity.findViewById(R.id.total);
+        assertEquals("0", textView.getText());
+    }
+
+    private SQLiteOpenHelper getAllowanceDatabase() {
+        return new AllowanceDatabase(activity);
     }
 
 }
