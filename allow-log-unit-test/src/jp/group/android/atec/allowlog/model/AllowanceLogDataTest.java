f7aef927938df1228751bbd210900f0d12d58cd5
ussy <ussy00@gmail.com>
2012-02-05 22:24:00 +0900

    DEVSUMI-18 SQLite Fixture Library を適用

--- allow-log-unit-test/src/jp/group/android/atec/allowlog/model/AllowanceLogDataTest.java
+++ allow-log-unit-test/src/jp/group/android/atec/allowlog/model/AllowanceLogDataTest.java
@@ -2,42 +2,28 @@
 package jp.group.android.atec.allowlog.model;
 
 import android.database.sqlite.SQLiteDatabase;
-import android.test.AndroidTestCase;
-import android.test.RenamingDelegatingContext;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import jp.group.android.atec.allowlog.AllowanceDatabase;
+import jp.group.android.atec.sf.importer.FileType;
+import jp.group.android.atec.sf.unit.DatabaseTestCase;
 
-public class AllowanceLogDataTest extends AndroidTestCase {
+public class AllowanceLogDataTest extends DatabaseTestCase {
 
-    private AllowanceDatabase helper;
+    @Override
+    protected SQLiteOpenHelper createSQLiteOpenHelper() {
+        return new AllowanceDatabase(getDatabaseContext());
+    }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
-        helper = new AllowanceDatabase(new RenamingDelegatingContext(getContext(), "test_"));
-
-        SQLiteDatabase db = helper.getWritableDatabase();
-        try {
-            db.execSQL("INSERT INTO ALLOWANCE_LOG(LOG_DATE, AMOUNT) VALUES(1320000000, 1000);");
-            db.execSQL("INSERT INTO ALLOWANCE_LOG(LOG_DATE, AMOUNT) VALUES(1330000000, 5000);");
-            db.execSQL("INSERT INTO ALLOWANCE_LOG(LOG_DATE, AMOUNT) VALUES(1340000000, 10000);");
-            db.execSQL("INSERT INTO ALLOWANCE_LOG(LOG_DATE, AMOUNT) VALUES(1350000000, 2000);");
-            db.execSQL("INSERT INTO ALLOWANCE_LOG(LOG_DATE, AMOUNT) VALUES(1360000000, 3000);");
-        } finally {
-            db.close();
-        }
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-        
-        helper.close();
+        importData(FileType.Yaml, "AllowanceLogDataTest");
     }
     
     public void testすべての期間の合計金額を計算() {
-        SQLiteDatabase database = helper.getReadableDatabase();
+        SQLiteDatabase database = getSQLiteDatabase();
         long totalAmount = AllowanceLogData.getTotalAmount(database, 0, Long.MAX_VALUE);
         assertEquals("すべての金額を足したもの", 21000, totalAmount);
         
@@ -45,7 +31,7 @@ public class AllowanceLogDataTest extends AndroidTestCase {
     }
     
     public void test期間内の合計金額を計算() {
-        SQLiteDatabase database = helper.getReadableDatabase();
+        SQLiteDatabase database = getSQLiteDatabase();
         long totalAmount = AllowanceLogData.getTotalAmount(database, 1330000000, 1350000000);
         assertEquals("期間間の金額を足したもの", 17000, totalAmount);
         
@@ -53,7 +39,7 @@ public class AllowanceLogDataTest extends AndroidTestCase {
     }
     
     public void test期間登録外の合計金額を計算() {
-        SQLiteDatabase database = helper.getReadableDatabase();
+        SQLiteDatabase database = getSQLiteDatabase();
         long totalAmount = AllowanceLogData.getTotalAmount(database, 1000000000, 1319999999);
         assertEquals("期間間の金額を足したもの", 0, totalAmount);
         
@@ -61,7 +47,7 @@ public class AllowanceLogDataTest extends AndroidTestCase {
     }
     
     public void testFromToの引数を間違えて計算() {
-        SQLiteDatabase database = helper.getReadableDatabase();
+        SQLiteDatabase database = getSQLiteDatabase();
         long totalAmount = AllowanceLogData.getTotalAmount(database, 1350000000, 1330000000);
         assertEquals("期間間の金額を足したもの", 0, totalAmount);
         
