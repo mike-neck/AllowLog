8c0b045b6744d27a2b254bb023a7064302fd4259
mike_neck <jkrt3333@gmail.com>
2012-02-08 21:33:04 +0900

    DEVSUMI-25 fix synchronization via https://atec.backlog.jp/view/DEVSUMI-25

--- allow-log-unit-test/src/jp/group/android/atec/allowlog/MainActivityTest.java
+++ allow-log-unit-test/src/jp/group/android/atec/allowlog/MainActivityTest.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 /**
  * 初回起動時のViewに関するテスト<br/>
  *
- * <ul>
+ * <ul style="list-style-type : numeric;">
  *     <li>使用金額が０円</li>
  *     <li>今月使用された金額が０円</li>
  * </ul>
@@ -57,6 +57,14 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     public void test今月はまだ一度も使っていないので０円 () {
+        activity.runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        instrumentation.callActivityOnResume(activity);
+                    }
+                }
+        );
         instrumentation.waitForIdleSync();
         TextView textView = (TextView) activity.findViewById(R.id.total);
         assertEquals("0", textView.getText());
