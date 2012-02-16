850067e4c0b093faa4af473b8ef78f3ce82b8127
mike_neck <jkrt3333@gmail.com>
2012-02-17 05:10:35 +0900

    DEVSUMI-33 refactor MainActivity via https://atec.backlog.jp/view/DEVSUMI-33

--- allow-log-app/src/jp/group/android/atec/allowlog/MainActivity.java
+++ allow-log-app/src/jp/group/android/atec/allowlog/MainActivity.java
@@ -1,15 +1,11 @@
 
 package jp.group.android.atec.allowlog;
 
-import java.util.Calendar;
-import java.util.TimeZone;
-
 import jp.group.android.atec.allowlog.model.AllowanceLogData;
+import jp.group.android.atec.allowlog.util.AppDateUtil;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -45,10 +41,10 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onResume();
 
         long totalValue = 0;
-        SQLiteDatabase database = prepareSql();
+        SQLiteDatabase database = AllowanceLogData.readableDatabase(this);
         try {
-            long from = getFromDate();
-            long to = getToDate();
+            long from = AppDateUtil.thisMonthFirst();
+            long to = AppDateUtil.thisMonthEnd();
             totalValue = AllowanceLogData.getTotalAmount(database, from, to);
         } finally {
             database.close();
@@ -56,45 +52,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
         TextView total = (TextView) findViewById(R.id.total);
         total.setText(Long.toString(totalValue));
-    }
-
-    long getTotalAmount(SQLiteDatabase sqLiteDatabase, String sql) {
-        Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
-        cursor.moveToFirst();
-        int count = cursor.getCount();
-        if ( count > 0 ) {
-            long totalValue = cursor.getLong(0);
-            return totalValue;
-        } else {
-            return 0L;
-        }
-    }
-
-    private SQLiteDatabase prepareSql() {
-        SQLiteOpenHelper database = new AllowanceDatabase(this);
-        return database.getReadableDatabase();
-    }
-
-    private long getToDate() {
-        Calendar toCalendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
-        toCalendar.add(Calendar.MONTH, 1);
-        toCalendar.set(Calendar.DATE, 1);
-        toCalendar.set(Calendar.HOUR_OF_DAY, 0);
-        toCalendar.set(Calendar.MINUTE, 0);
-        toCalendar.set(Calendar.SECOND, 0);
-        toCalendar.set(Calendar.MILLISECOND, 0);
-        toCalendar.add(Calendar.MILLISECOND, -1);
-        return toCalendar.getTimeInMillis();
-    }
-
-    private long getFromDate() {
-        Calendar fromCalendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
-        fromCalendar.set(Calendar.DATE, 1);
-        fromCalendar.set(Calendar.HOUR_OF_DAY, 0);
-        fromCalendar.set(Calendar.MINUTE, 0);
-        fromCalendar.set(Calendar.SECOND, 0);
-        fromCalendar.set(Calendar.MILLISECOND, 0);
-        return fromCalendar.getTimeInMillis();
     }
 
     /**
